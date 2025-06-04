64f9d07d51088852cef3fb2aa4143f3ddbc27034
hziqzmin <muhd.haziqazmin@gmail.com>
2025-06-04 18:13:04 +0900

    new ezRun Web App

---
+++ src/main/java/com/dongyang/ezRun/EzRunApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.ezRun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzRunApplication.class, args);
+	}
+
+}
